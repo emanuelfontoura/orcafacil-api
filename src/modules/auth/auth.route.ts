0eb65d30fa75f968f64dd6620a64e04f21cce986
Emanuel Victor Fontoura Cantalejo <emanuelfontoura03@gmail.com>
2026-04-01 23:39:06 -0300

    feat(middleware): criado middleware de rate limit para requisições
    criado middleware que contabiliza as requisições utilizando a memória cache via Redis e valida a sua quantidade
    
    BREAKING CHANGE: rotas auth suportam até 3 requisições por minuto

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { AuthUserController } from './auth.controller'
 import { validateSchema } from "../../middlewares/validate";
 import { authSchemas } from "./auth.schema"
+import { rateLimit } from "../../middlewares/rateLimit";
 
 const authUserRoutes = Router()
 
-authUserRoutes.post('/email/verify', validateSchema(authSchemas.verifyEmailSchema), AuthUserController.verifyEmail)
-authUserRoutes.post('/email/confirm', validateSchema(authSchemas.confirmEmailSchema), AuthUserController.confirmEmail)
+authUserRoutes.post('/email/verify', rateLimit(3, 60, 'auth/email/verify'), validateSchema(authSchemas.verifyEmailSchema), AuthUserController.verifyEmail)
+authUserRoutes.post('/email/confirm', rateLimit(3, 60, 'auth/email/confirm'), validateSchema(authSchemas.confirmEmailSchema), AuthUserController.confirmEmail)
 
 export { authUserRoutes }

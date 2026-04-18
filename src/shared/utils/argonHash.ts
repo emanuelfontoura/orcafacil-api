2e205b1d8c5a08efcb8e6166f12eaff279214609
Emanuel Victor Fontoura Cantalejo <emanuelfontoura03@gmail.com>
2026-04-18 15:42:56 -0300

    refactor(auth): criada classe de ArgonHash para unir todos os processos envolvendo hash.

--- src/shared/utils/argonHash.ts
+++ src/shared/utils/argonHash.ts
@@ -1,28 +1,30 @@
 import argon2 from "argon2"
 import "dotenv/config"
-
-const argonConfigHash = {
-    type: argon2.argon2id,
-    memoryCost: 65536,
-    timeCost: 3,
-    parallelism: 1
-}
-
-const PEPPER = process.env.PEPPER
+import { env } from "@/config/env"
 
 export class ArgonHash{
+
+    private static argonConfigHash: argon2.Options = {
+        type: argon2.argon2id,
+        memoryCost: 65536,
+        timeCost: 3,
+        parallelism: 1
+    }
+
+    private static PEPPER: string = env.PEPPER
+
     static async argonHash(valueToHash: string): Promise<string>{
-        const hashedValue = await argon2.hash(valueToHash + PEPPER, argonConfigHash)
+        const hashedValue = await argon2.hash(valueToHash + this.PEPPER, this.argonConfigHash)
         return hashedValue
     }
 
     static async argonVerify(hashedValue: string, normalValue: string): Promise<boolean>{
-        const matchValues = await argon2.verify(hashedValue, normalValue + PEPPER)
+        const matchValues = await argon2.verify(hashedValue, normalValue + this.PEPPER)
         return matchValues
     }
 
     static async argonRehash(normalValue: string, hashedValue: string): Promise<string | null>{
-        if(!argon2.needsRehash(hashedValue)) return null
+        if(!argon2.needsRehash(hashedValue, this.argonConfigHash)) return null
         return await this.argonHash(normalValue)
     }
 }
